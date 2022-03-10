f32dfaaf1a8da94d1c94a4504664d42647f7b40e
Monique McIntyre <skaiwilliams85@gmail.com>
2022-03-09 23:00:26 -0500

    added styling for the card

--- src/components/countries/Country.js
+++ src/components/countries/Country.js
@@ -1,60 +1,33 @@
 import React from "react";
+import styled from "./Country.module.css";
 
 const Country = ({ countries }) => {
   // console.log(countries);
   let size = 20;
 
-  /*
-  const random = [];
-  let randomSet = new Set();
-
-  for (let i = 0; i <= 20; i++) {
-    random.push(Math.floor(Math.random() * 250));
-    randomSet = [...random];
-  }
-  console.log(randomSet);
-  
-
-    const test = randomSet.map((numm) => {
-      return countries.map((place) => {
-        return <p key={place.name.official}>{place[numm]}</p>;
-      });
-    });
-
-    const test = countries.map((place) => {
-      return randomSet.map((numm) => {
-        return <p>{place[numm].name.official}</p>;
-      });
-    });
-  const name = [];
-  for (let x = 0; x < randomSet.size; x++) {
-    name.push(countries[randomSet[x]].name.official);
-  }
-
-  console.log(name);
-  */
   //TODO reduce load time
   //TODO select random countries of the size above
 
   const countryDetails = countries
     .slice(0, size)
     .map(({ flags, name, capital, population, region }) => (
-      <article key={name.official} className="my-4">
-        <section>
+      <article key={name.official} className={styled.article}>
+        <section className={styled.flag}>
           <img src={flags.png} alt="flag" />
         </section>
-        <section>
-          <h3 className="name">{name.official}</h3>
+
+        <section className={styled.details}>
+          <h3 className={styled.name}>{name.official}</h3>
           <p>
-            <span className="population">Population: </span>
+            <span className={styled.population}>Population: </span>
             {population}
           </p>
           <p>
-            <span className="region">Region: </span>
+            <span className={styled.region}>Region: </span>
             {region}
           </p>
           <p>
-            <span className="capital">Capital:</span>
+            <span className={styled.capital}>Capital:</span>
             {capital}
           </p>
         </section>
