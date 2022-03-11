2beb31ef1caf2f040c1284663712dead42cda037
Monique McIntyre <skaiwilliams85@gmail.com>
2022-03-10 23:11:11 -0500

    styled dropdown

--- src/components/input/Dropdown.js
+++ src/components/input/Dropdown.js
@@ -1,18 +1,21 @@
 import React from "react";
+import styled from "./Dropdown.module.css";
 
 const Dropdown = () => {
   return (
-    <select>
-      <option selected hidden>
-        Filter by Region
-      </option>
-      <option value="Africa">Africa</option>
-      <option value="America">America</option>
-      <option value="Asia">Asia</option>
-      <option value="Asia">Asia</option>
-      <option value="Europe">Europe</option>
-      <option value="Oceania">Oceania</option>
-    </select>
+    <section className={styled.dropdown}>
+      <select className={styled.menu}>
+        <option selected hidden>
+          Filter by Region
+        </option>
+        <option value="Africa">Africa</option>
+        <option value="America">America</option>
+        <option value="Asia">Asia</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+    </section>
   );
 };
 
