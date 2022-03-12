10f3f02c41472d293a6ed306d82d7b3d5014f564
Monique McIntyre <skaiwilliams85@gmail.com>
2022-03-11 20:06:57 -0500

    rendered the country details

--- src/components/countries/Details.js
+++ src/components/countries/Details.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 const Details = ({ countries }) => {
   const { countryName } = useParams();
   let nation = [];
+  let lang = "";
 
   for (let place of countries) {
     if (place.name.official === countryName) {
@@ -12,7 +13,75 @@ const Details = ({ countries }) => {
   }
 
   nation !== [] && console.log(nation);
-  return <div>Details</div>;
+
+  const nationDetails = nation.map((country) => {
+    for (let x of Object.values(country.languages)) {
+      lang += x + " ";
+    }
+    console.log(lang);
+    return (
+      <section key={country.name.official}>
+        <button>Back</button>
+
+        <article>
+          <figure>
+            <img src="" alt="" />
+          </figure>
+          <section>
+            <h3>{country.name.official}</h3>
+
+            <div>
+              <p>
+                <span>Native Name: </span>
+                {country.name.nativeName.nld.common}
+              </p>
+              <p>
+                <span>Population: </span>
+                {country.population}
+              </p>
+              <p>
+                <span>Region: </span>
+                {country.region}
+              </p>
+              <p>
+                <span>Sub Region: </span>
+                {country.subregion}
+              </p>
+              <p>
+                <span>Capital: </span>
+                {country.capital}
+              </p>
+            </div>
+
+            <div>
+              <p>
+                <span>Top Level Domain: </span>
+                {country.tld}
+              </p>
+              <p>
+                <span>Currencies: </span>
+                {country.currencies.name}
+              </p>
+              <p>
+                <span>Languages: </span>
+                {lang}
+              </p>
+            </div>
+
+            <>
+              <h5>Border Countries:</h5>
+              <div>
+                {country.borders.map((border) => {
+                  return <button>{border} </button>;
+                })}
+              </div>
+            </>
+          </section>
+        </article>
+      </section>
+    );
+  });
+  return <div>{nation !== [] && nationDetails}</div>;
 };
 
 export default Details;
